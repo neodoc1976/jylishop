a352655a03e608807a0ddaa3cd787bfb6bd4a436
George <neodoc3.gmail.com>
2016-05-21 21:18:22 +0300

    Add Hemo And Gel Product Is Possible

--- src/main/java/org/george/jylishop/controllers/AdminGelController.java
+++ src/main/java/org/george/jylishop/controllers/AdminGelController.java
@@ -1,54 +1,56 @@
-//package org.george.jylishop.controllers;
-//
-//import org.george.jylishop.db.DataBase;
-//import org.george.jylishop.domain.OpalescenseGel;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * Created by Yulya on 03.05.2016.
-// */
-//@Controller
-//public class AdminGelController {
-//    @Autowired
-//    DataBase base;
-//
-//    @RequestMapping(value = "/admin/gels/add", method = RequestMethod.GET)
-//    public ModelAndView getForm() {
-//        ModelAndView view = new ModelAndView("admin-add-gel");
-//        return view;
-//    }
-//
-//    @RequestMapping(value = "/admin/gels", method = RequestMethod.POST)
-//    public ModelAndView postForm(@RequestParam String title, @RequestParam Double volume,
-//                                 @RequestParam Double price, @RequestParam Integer reactantPercent, @RequestParam String description) {
-//        ModelAndView post = new ModelAndView("admin-gel-list");
-//        // post.addObject("title", title);
-//        // post.addObject("reactantPercent", reactantPercent);
-//        // post.addObject("volume", volume);
-//        // post.addObject("price", price);
-//        //  post.addObject("description", description);
-//        OpalescenseGel added = new OpalescenseGel();
-//        post.addObject("catalogue", base.getCatalogue());
-//        added.setTitle(title);
-//        added.setDescription(description);
-//        added.setVolume(volume);
-//        added.setReactantPercent(reactantPercent);
-//        added.setPrice(price);
-//        List<OpalescenseGel> list = base.getCatalogue();
-//        list.add(added);
-//        return post;
-//
-//    }
-//
+package org.george.jylishop.controllers;
+
+        import org.george.jylishop.db.DataBase;
+        import org.george.jylishop.domain.OpalescenseGel;
+        import org.george.jylishop.domain.Product;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.servlet.ModelAndView;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+/**
+ * Created by Yulya on 03.05.2016.
+ */
+@Controller
+public class AdminGelController {
+    @Autowired
+    DataBase base;
+
+    @RequestMapping(value = "/admin/gels/add", method = RequestMethod.GET)
+    public ModelAndView getForm() {
+        ModelAndView view = new ModelAndView("admin-add-gel");
+        return view;
+    }
+
+    @RequestMapping(value = "/admin/gels", method = RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam String title,
+                                 @RequestParam Double volume,
+                                 @RequestParam Double price,
+                                 @RequestParam Integer reactantPercent,
+                                 @RequestParam String description,
+                                 @RequestParam Integer id)
+    {
+        ModelAndView post = new ModelAndView("admin-total");
+        OpalescenseGel added = new OpalescenseGel();
+        post.addObject("catalogue", base.getCatalogue());
+        added.setTitle(title);
+        added.setDescription(description);
+        added.setVolume(volume);
+        added.setReactantPercent(reactantPercent);
+        added.setPrice(price);
+        added.setId(id);
+        List<Product> list = base.getCatalogue();
+        list.add(added);
+        return post;
+
+    }
+
 //    @RequestMapping(value = "/admin/gels/{id}/delete", method = RequestMethod.GET)
 //    public ModelAndView deleteForm(@PathVariable int id) {
 //        ModelAndView delete = new ModelAndView("admin-gel-list");
@@ -95,6 +97,6 @@
 //
 //
 //    }
-//
-//
-//}
+
+
+}
--- src/main/java/org/george/jylishop/controllers/AdminHemoController.java
+++ src/main/java/org/george/jylishop/controllers/AdminHemoController.java
@@ -1,54 +1,57 @@
-//package org.george.jylishop.controllers;
-//
-//
-//        import org.george.jylishop.db.DataBase;
-//        import org.george.jylishop.domain.Hemostatic;
-//        import org.george.jylishop.domain.OpalescenseGel;
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.stereotype.Controller;
-//        import org.springframework.web.bind.annotation.PathVariable;
-//        import org.springframework.web.bind.annotation.RequestMapping;
-//        import org.springframework.web.bind.annotation.RequestMethod;
-//        import org.springframework.web.bind.annotation.RequestParam;
-//        import org.springframework.web.servlet.ModelAndView;
-//
-//        import java.util.ArrayList;
-//        import java.util.List;
-//
-///**
-// * Created by Yulya on 21.05.2016.
-// */
-//@Controller
-//public class AdminHemoController {
-//    @Autowired
-//    DataBase base;
-//
-//    @RequestMapping(value = "/admin/hemos/add", method = RequestMethod.GET)
-//    public ModelAndView getForm() {
-//        ModelAndView view = new ModelAndView("admin-add-hemo");// Stop here
-//        return view;
-//     }
-//
-//    @RequestMapping(value = "/admin/hemos", method = RequestMethod.POST)//Stop here
-//    public ModelAndView postForm(@RequestParam String title,
-//                                 @RequestParam Double volume,
-//                                 @RequestParam Double price,
-//                                 @RequestParam String hemostaticSubstance,
-//                                 @RequestParam String description) {
-//        ModelAndView post = new ModelAndView("admin-hemos-list");//Stop here
-//        Hemostatic newcomer = new Hemostatic();
-//        post.addObject("hemolist", base.getHemolist());
-//        newcomer.setTitle(title);
-//        newcomer.setDescription(description);
-//        newcomer.setVolume(volume);
-//        newcomer.setHemostaticSubstance(hemostaticSubstance);
-//        newcomer.setPrice(price);
-//        List<Hemostatic> list = base.getHemolist();
-//        list.add(newcomer);
-//        return post;
-//
-//    }
-//
+package org.george.jylishop.controllers;
+
+
+        import org.george.jylishop.db.DataBase;
+        import org.george.jylishop.domain.Hemostatic;
+        import org.george.jylishop.domain.OpalescenseGel;
+        import org.george.jylishop.domain.Product;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.servlet.ModelAndView;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+/**
+ * Created by Yulya on 21.05.2016.
+ */
+@Controller
+public class AdminHemoController {
+    @Autowired
+    DataBase base;
+
+    @RequestMapping(value = "/admin/hemos/add", method = RequestMethod.GET)
+    public ModelAndView getForm() {
+        ModelAndView view = new ModelAndView("admin-add-hemo");// Stop here
+        return view;
+     }
+
+    @RequestMapping(value = "/admin/hemos", method = RequestMethod.POST)//Stop here
+    public ModelAndView postForm(@RequestParam String title,
+                                 @RequestParam Double volume,
+                                 @RequestParam Double price,
+                                 @RequestParam String substance,
+                                 @RequestParam String description,
+                                 @RequestParam Integer id) {
+        ModelAndView post = new ModelAndView("admin-total");//Stop here
+        Hemostatic newcomer = new Hemostatic();
+        post.addObject("catalogue", base.getCatalogue());
+        newcomer.setTitle(title);
+        newcomer.setDescription(description);
+        newcomer.setVolume(volume);
+        newcomer.setHemostaticSubstance(substance);
+        newcomer.setPrice(price);
+        newcomer.setId(id);
+        List<Product> list = base.getCatalogue();
+        list.add(newcomer);
+        return post;
+
+    }
+
 //    @RequestMapping(value = "/admin/hemos/{id}/delete", method = RequestMethod.GET)//Stop here
 //    public ModelAndView deleteForm(@PathVariable int id) {
 //        ModelAndView delete = new ModelAndView("admin-hemos-list");
@@ -96,8 +99,8 @@
 //
 //    }
 //
-//
-//}
-//
-//
-//
+
+}
+
+
+
